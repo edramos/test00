ca5fecb7652fba0722ecd0104b3fdc15a7d31e16
Ed <ed@brilyint.com>
2015-01-09 00:23:12 -0500

    Mensajes UI - Controller

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,6 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-
 <!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <html>
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
@@ -11,6 +12,16 @@
     <div id="content" class="container">
         <h1>Hello Eduardo!</h1>
         <p><a href="userGetSaludo">Saludo User</a></p>
+    </div>
+    
+    <div>
+    	<form:form action="getMessage" method="post" commandName="userBean">
+    		<table border = 1 align="center">
+    			<tr><td><label>Nombre: </label></td><td><input type="text" name="nombre"/></td></tr>
+    			<tr><td><label>Email: </label></td><td><input type="text" name="email"/></td></tr>
+    			<tr><td></td><td><input type="submit" value="Enviar!"/></td></tr>
+    		</table>
+    	</form:form>
     </div>
 </body>
 </html>
--- src/main/java/com/simularte/controller/UserController.java
+++ src/main/java/com/simularte/controller/UserController.java
@@ -3,13 +3,15 @@ package com.simularte.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.simularte.form.UserBean;
 import com.simularte.service.UserService;
 
 @Controller
-public class UserC {
+public class UserController {
 
 	@Autowired
 	UserService us;
@@ -19,8 +21,25 @@ public class UserC {
 		String path = "";
 		
 		//model.addAttribute("saludo", "Buenas Madrugadas Massimo");
-		
 		model.addAttribute("saludo", us.saludar());
+		
+		path = "saludo";
+		
+		return path;
+	}
+	
+	@RequestMapping(value = "getMessage", method = RequestMethod.POST)
+	public String getMessage(@ModelAttribute("userBean") UserBean userBean, Model model){
+		String path = "";
+			
+		String mensaje = "Buenas Madrugadas " + userBean.getNombre();
+			
+		if(userBean.getEmail()!= ""){
+			mensaje += ", tu email es " + userBean.getEmail();
+		}else{
+			mensaje += ", creo que no tienes email";
+		}
+		model.addAttribute("saludo", mensaje);
 		
 		path = "saludo";
 		
